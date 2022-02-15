e01071a1f21ddca2f02f1c3b2f04bcd7d587ebd5
unknown <qwhick2@gmail.com>
2022-02-15 17:40:00 -0500

    Tested GET request, Success

--- products/views.py
+++ products/views.py
@@ -1,13 +1,20 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+
+import products
+from .serializers import ProductSerializer
+from .models import Product
+from products import serializers
 
 # Create your views here.
 
 @api_view(['GET'])
 def products_list(request):
-
-    print('good job')
-    return Response('good job')
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view([])
 def product_detail():
