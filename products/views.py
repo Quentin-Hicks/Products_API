ac81cb5036baedc10c67e1f1107bb3e292b401cd
unknown <qwhick2@gmail.com>
2022-02-15 16:06:21 -0500

    Created products_list and product_detail Functions + Added Path for pk

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
+def products_list(request):
+
+    print('good job')
+    return Response('good job')
+
+@api_view([])
+def product_detail():
+    pass
